d87ef528ffdeee3088e747a2ff2cd8ec598a9ac7
BrockStech <stechsbj@mail.uc.edu>
2019-12-06 17:02:07 -0500

    Add files via upload

--- serial_a_star.py
+++ serial_a_star.py
@@ -1,13 +1,16 @@
 import link_parser
+import time
+import sys
 
 unused = []
-endPage = '/wiki/University_of_Utah_School_of_Medicine'
 
 def pathToPage(page):
+    print('Most efficient path to end page:')
     path = [page.page_url]
     while page.parent != '':
         path.append(page.parent.page_url)
         page = page.parent
+    path.reverse()
     return path
 
 def heuristic(page):
@@ -20,7 +23,7 @@ def initSubpage(page, subpageLink):
     page2.heuristic = heuristic(page2)
     return page2
 
-def findPage(page):
+def findPage(page, endPage):
     nextPageHeuristic = 1
     if unused != []:
         unused.sort(key=lambda x: x.heuristic)
@@ -41,15 +44,45 @@ def findPage(page):
     print(nextPage.page_url)
     return nextPage
 
+def checkArguments():
+    if (len(sys.argv) == 2):
+        return False
+    return True
+
+def defaultPages():
+    startURL = 'https://en.wikipedia.org/wiki/'
+    endPage = '/wiki/'
+    useDefaultURLs = checkArguments()
+    if (useDefaultURLs):
+        print('No arguments passed for start page and end page.\nUsing default pages...\n')
+        startURL = startURL + 'Handsfree'
+        endPage = endPage + 'University_of_Utah_Honors_College'
+    else:
+        startURL = startURL + sys.argv[1]
+        endPage = endPage + sys.argv[2]
+    defaultPages = [startURL, endPage]
+    return defaultPages
+
 def main():
-    startURL = 'https://en.wikipedia.org/wiki/Handsfree'
-    page = link_parser.Page(startURL)
+    wikiPages = defaultPages()
+    startURL = wikiPages[0]
+    endPage = wikiPages[1]
+        
+    start_time = time.time()
+    
+    try:
+        page = link_parser.Page(startURL)
+    except:
+        print('Start page or end page is not valid!\nPlease ensure page name is correct\nExample Arguments: Handsfree University_of_Utah_Honors_College')
+        return
+        
     if endPage in page.links:
         print('Starting page already contains the end page link')
         return
     
     while page:
-        page = findPage(page)
+        page = findPage(page, endPage)
      
+    print("Total Time: %s seconds " % (time.time() - start_time))
 if __name__ == '__main__':
     main()
